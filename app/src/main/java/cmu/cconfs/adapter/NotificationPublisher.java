e6eac3153fdac4f146ef4e3ca37861856e98c2f8
GuoQiwen <Wendy_Guo@GuoQiwens-MBP.sv.cmu.local>
2015-11-11 15:29:03 -0800

    notify

---
+++ app/src/main/java/cmu/cconfs/adapter/NotificationPublisher.java
@@ -0,0 +1,17 @@
+package cmu.cconfs.adapter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationPublisher extends BroadcastReceiver {
+    public NotificationPublisher() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
